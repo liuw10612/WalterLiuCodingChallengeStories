f84cc56022bea38781426854c9f709420bc42c67
liuw10612 <liuw10612@msn.com>
2023-07-13 15:17:16 -0400

    works fa icon, filter pipe

--- src/NexTechCodingChallengeStories.Web/ClientApp/src/app/new-stories/new-stories.component.ts
+++ src/NexTechCodingChallengeStories.Web/ClientApp/src/app/new-stories/new-stories.component.ts
@@ -16,26 +16,39 @@ export class NewStoriesDataComponent  {
   perPage = 10;
   calculateTotalPages = this.collectionSize / this.perPage;
 
+  public loading: boolean = false;
+  public searchText: string = '';
+
   constructor(  private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
     this.getNewStoriesCount();
   }
   
   // get stories count
   public getNewStoriesCount() {
+    this.loading = true;
     this.http.get<number>(this.baseUrl + `codechallenge/storiesCount`)
       .subscribe(result => {
+        this.loading = false;
         this.collectionSize = result;
         this.calculateTotalPages = this.collectionSize / this.perPage;
         this.loadOnePage(1);  
-      }, error => console.error(error));
+      }, error => {
+        this.loading = false;
+        console.error(error);
+      });
 
   }
   // setup 1st page
   public loadOnePage(currentPage: number) {
+    this.loading = true;
     this.http.get<StroyTitle[]>(`${this.baseUrl}codechallenge/onePage?p=${currentPage}&ps=${this.perPage}`)
       .subscribe(result => {
+        this.loading = false;
         this.stories = result;
-      }, error => console.error(error));
+      }, error => {
+        this.loading = false;
+        console.error(error);
+      });
 
   }
   public onPageChange(event: Event) {
